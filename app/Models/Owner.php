f4d361f63b19dcfe3c30326546020cc3c7355666
muathye <muath.ye@gmail.com>
2023-07-29 05:34:02 +0300

    feat: add models with migrations

---
+++ app/Models/Owner.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Owner extends Model
+{
+    use HasFactory;
+
+    public function patients(): HasMany
+    {
+        return $this->hasMany(Patient::class);
+    }
+}
---
+++ app/Models/Patient.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Patient extends Model
+{
+    use HasFactory;
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(Owner::class);
+    }
+
+    public function treatments(): HasMany
+    {
+        return $this->hasMany(Treatment::class);
+    }
+}
