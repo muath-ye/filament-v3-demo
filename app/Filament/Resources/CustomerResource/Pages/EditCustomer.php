d1862672d1496fa9396d94dcd62227545ea18b24
muathye <muath.ye@gmail.com>
2023-07-29 13:33:18 +0300

    feat: Editing records

--- app/Filament/Resources/CustomerResource/Pages/EditCustomer.php
+++ app/Filament/Resources/CustomerResource/Pages/EditCustomer.php
@@ -4,7 +4,9 @@ namespace App\Filament\Resources\CustomerResource\Pages;
 
 use App\Filament\Resources\CustomerResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class EditCustomer extends EditRecord
 {
@@ -18,5 +20,27 @@ class EditCustomer extends EditRecord
             Actions\ForceDeleteAction::make(),
             Actions\RestoreAction::make(),
         ];
+    }
+
+    protected function handleRecordUpdate(Model $record, array $data): Model
+    {
+        $data['password'] = bcrypt($data['password']);
+
+        $record->update($data);
+
+        return $record;
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('view', ['record' => $this->getRecord()]);
+    }
+
+    protected function getSavedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('User updated')
+            ->body('The user has been saved successfully.');
     }
 }
